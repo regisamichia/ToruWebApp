94256cca90eec4cbaa87502176fb424acc876292
Régis Amichia <regis@foxintelligence.fr>
2024-10-03 17:53:24 +0200

    save audio from eleven labs

--- app/routes/text_to_speech.py
+++ app/routes/text_to_speech.py
@@ -1,10 +1,11 @@
 import os
 from pydantic import BaseModel
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, BackgroundTasks
 from fastapi.responses import  StreamingResponse
 import requests
 import logging
 import io
+import boto3
 
 
 router = APIRouter()
@@ -15,9 +16,22 @@ logger = logging.getLogger(__name__)
 
 class TextToSpeechRequest(BaseModel):
     text: str
+    user_id: str
+    message_id: str
+
+s3_client = boto3.client('s3')
+bucket_name = 'toruchat'
+
+# Function to upload audio to S3 in the background
+def upload_audio_to_s3(audio_bytes: bytes, s3_key: str):
+    try:
+        s3_client.upload_fileobj(io.BytesIO(audio_bytes), bucket_name, s3_key)
+        print(f"File uploaded successfully to s3://{bucket_name}/{s3_key}")
+    except Exception as e:
+        print(f"Error uploading file to S3: {e}")
 
 @router.post("/api/synthesize_audio")
-async def synthesize_audio_endpoint(request: TextToSpeechRequest):
+async def synthesize_audio_endpoint(request: TextToSpeechRequest, background_tasks: BackgroundTasks):
     try:
         headers = {
             "xi-api-key": os.getenv("ELEVEN_API_KEY"),
@@ -46,6 +60,10 @@ async def synthesize_audio_endpoint(request: TextToSpeechRequest):
         if response.status_code != 200:
             logger.error(f"Eleven Labs API error: {response.text}")
             raise HTTPException(status_code=response.status_code, detail="Error from Eleven Labs API")
+
+        # Add a background task to upload the audio to S3
+        s3_key = f"{request.user_id}/{request.message_id}.mp3"
+        background_tasks.add_task(upload_audio_to_s3, response.content, s3_key)
 
         # Return the audio as a streaming response
         return StreamingResponse(io.BytesIO(response.content), media_type="audio/mpeg")
